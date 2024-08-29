d14131dfcf52b56b60b95fc46cf8b43c49382c9e
CoderBeast-dev <arv.sarna29@gmail.com>
2024-08-29 14:04:04 +0530

    face.py

--- face.py
+++ face.py
@@ -7,7 +7,7 @@ import os
 from datetime import datetime
 
 engine = pyttsx3.init()
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 path = "photos"
 sys = os.listdir(path)
@@ -15,7 +15,8 @@ sys = os.listdir(path)
 known_face_encoding = []
 
 for i in sys:
-    known_face_encoding.append(face_recognition.face_encodings(face_recognition.load_image_file('photos/'+i))[0])
+    known_face_encoding.append(face_recognition.face_encodings(face_recognition.load_image_file('photos/' + i))[0])
+
 
 known_faces_names = sys
 
@@ -31,26 +32,32 @@ current_date = now.strftime("%Y-%m-%d")
 
 f = open(current_date + '.csv', 'w+', newline='')
 lnwriter = csv.writer(f)
+count = 0
+cooldown = 50
 
+face_names = []
 while True:
     _, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
 
-    if s:
+    if (count % 15 == 0):
+
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-        face_names = []
+        
         for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
+            matches = face_recognition.compare_faces(known_face_encoding, face_encoding, tolerance=0.6)
             name = ""
+            face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
+            best_match_index = np.argmin(face_distance)
+        
             face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
             best_match_index = np.argmin(face_distance)
             if matches[best_match_index]:
                 name = known_faces_names[best_match_index]
+                face_names.append(name)
 
-            face_names.append(name)
-            if name in known_faces_names:
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 bottomLeftCornerOfText = (10, 100)
                 fontScale = 1.5
@@ -73,8 +80,19 @@ while True:
                     print(students)
                     current_time = now.strftime("%H-%M-%S")
                     lnwriter.writerow([name.split('.')[0], current_time])
-            else:
-                engine.say("Not a part of this class")
+                else:
+                    engine.say("Already marked Present")
+                
+            flag = 0
+            for values in matches:
+                if values == True:
+                    flag+=1
+            if flag ==0:
+                print("Not a part of this class")
+                engine.say(name.split('.')[0] + "Not a part of this class")
+                engine.runAndWait()
+
+    count += 1
     cv2.imshow("attendence system", frame)
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
